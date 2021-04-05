f7d35c9a8019db434118e3c4cd03d3d907d22fe0
CourtHive <charles@courthive.com>
2021-04-05 06:02:28 +0200

    add tournament team processing

--- src/getPositionAssignmentHashes.js
+++ src/getPositionAssignmentHashes.js
@@ -2,28 +2,40 @@
 
 const getId = p => p?.id || p?.puid;
 
-export function getPositionAssignmentHashes({ matches = [] }) {
+export function getPositionAssignmentHashes({ matches = [], tournament }) {
   const positionHashMap = {};
   matches.forEach(legacyMatch => {
-    legacyMatch.teams?.forEach((team, index) => {
+    legacyMatch.teams?.forEach(team => {
       if (!team?.length) return;
-
-      const opponent1 =
-        team && team[0] && typeof team[0] === 'object' && team[0];
-      const opponent2 =
-        team && team[1] && typeof team[1] === 'object' && team[1];
-      const drawPosition = opponent1?.draw_position || opponent2?.draw_position;
-
-      if (drawPosition) {
-        if (getId(opponent1)) positionHashMap[getId(opponent1)] = drawPosition;
-        if (getId(opponent2)) positionHashMap[getId(opponent2)] = drawPosition;
-        if (opponent1.players) {
-          Object.keys(opponent1.players).forEach(playerId => {
-            positionHashMap[playerId] = drawPosition;
-          });
-        }
-      }
+      addToHash(team);
     });
+    if (legacyMatch.winner) addToHash(legacyMatch.winner);
+    if (legacyMatch.loser) addToHash(legacyMatch.loser);
   });
+
+  function addToHash(side) {
+    const opponent1 = side && side[0] && typeof side[0] === 'object' && side[0];
+    const opponent2 = side && side[1] && typeof side[1] === 'object' && side[1];
+    const drawPosition = opponent1?.draw_position || opponent2?.draw_position;
+    const addTeamPlayers = players => {
+      Object.keys(players).forEach(playerId => {
+        positionHashMap[playerId] = drawPosition;
+      });
+    };
+    const checkTournamentTeams = id => {
+      const team = tournament?.teams?.find(team => team.id === id);
+      if (team?.players) addTeamPlayers(team.players);
+    };
+
+    if (drawPosition) {
+      if (opponent1?.id) positionHashMap[opponent1.id] = drawPosition;
+      if (opponent2?.id) positionHashMap[opponent2.id] = drawPosition;
+      if (opponent1?.puid) positionHashMap[opponent1.puid] = drawPosition;
+      if (opponent2?.puid) positionHashMap[opponent2.puid] = drawPosition;
+      if (opponent1.players) addTeamPlayers(opponent1.players);
+      checkTournamentTeams(opponent1?.id);
+    }
+  }
+
   return positionHashMap;
 }
