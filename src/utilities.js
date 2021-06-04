7f58b6bf66d7c7dcfe3d9490b7237d2c48ebcedc
CourtHive <charles@courthive.com>
2021-06-04 17:52:27 +0200

    prettify

--- src/utilities.js
+++ src/utilities.js
@@ -2,13 +2,13 @@ import {
   eventConstants,
   genderConstants,
   drawDefinitionConstants,
-} from "tods-competition-factory";
+} from 'tods-competition-factory';
 
 const surfaceCategoryMap = {
-  C: "CLAY",
-  H: "HARD",
-  G: "GRASS",
-  R: "CARPET",
+  C: 'CLAY',
+  H: 'HARD',
+  G: 'GRASS',
+  R: 'CARPET',
 };
 
 export function getSurface(element) {
@@ -17,30 +17,30 @@ export function getSurface(element) {
 
 export function getIndoorOutdoor(element) {
   return (
-    (element?.inout === "o" && "OUTDOOR") ||
-    (element?.inout === "i" && "INDOOR")
+    (element?.inout === 'o' && 'OUTDOOR') ||
+    (element?.inout === 'i' && 'INDOOR')
   );
 }
 
 export function intersection(a, b) {
   return a
-    .filter((n) => b.indexOf(n) !== -1)
+    .filter(n => b.indexOf(n) !== -1)
     .filter((e, i, c) => c.indexOf(e) === i);
 }
 
 export function getAgeCategoryCode(category) {
   const categoryCodeMap = {
-    U10: "10U",
-    U12: "12U",
-    U14: "14U",
-    U16: "16U",
-    U18: "18U",
-    10: "10U",
-    12: "12U",
-    14: "14U",
-    16: "16U",
-    18: "18U",
-    Senior: "O18",
+    U10: '10U',
+    U12: '12U',
+    U14: '14U',
+    U16: '16U',
+    U18: '18U',
+    10: '10U',
+    12: '12U',
+    14: '14U',
+    16: '16U',
+    18: '18U',
+    Senior: 'O18',
   };
 
   return categoryCodeMap[category];
@@ -48,17 +48,17 @@ export function getAgeCategoryCode(category) {
 
 export function getMatchUpType(format) {
   return (
-    (["S", "SINGLES"].includes(format.toUpperCase()) &&
+    (['S', 'SINGLES'].includes(format.toUpperCase()) &&
       eventConstants.SINGLES) ||
-    (["D", "DOUBLES"].includes(format.toUpperCase()) && eventConstants.DOUBLES)
+    (['D', 'DOUBLES'].includes(format.toUpperCase()) && eventConstants.DOUBLES)
   );
 }
 
 export function getGender(value) {
   if (!value) return genderConstants.MIXED;
-  if (["F", "FEMALE", "W", "WOMAN"].includes(value.toUpperCase()))
+  if (['F', 'FEMALE', 'W', 'WOMAN'].includes(value.toUpperCase()))
     return genderConstants.FEMALE;
-  if (["M", "MALE", "MAN"].includes(value.toUpperCase()))
+  if (['M', 'MALE', 'MAN'].includes(value.toUpperCase()))
     return genderConstants.MALE;
   return genderConstants.MIXED;
 }
@@ -73,8 +73,8 @@ export function getStage({ legacyEvent }) {
     A: drawDefinitionConstants.MAIN,
   };
 
-  if (legacyEvent.draw_type === "R") {
-    if (Object.keys(legacyEvent.links || {}).includes("E")) {
+  if (legacyEvent.draw_type === 'R') {
+    if (Object.keys(legacyEvent.links || {}).includes('E')) {
       return drawDefinitionConstants.QUALIFYING;
     } else {
       return drawDefinitionConstants.MAIN;
