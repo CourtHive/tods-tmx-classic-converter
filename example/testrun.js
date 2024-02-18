47e9c09bd459bc5db8bd3ea00588a9a12bc7fb8e
courthive <charles@courthive.com>
2024-02-18 06:55:41 -0500

    feat: :technologist: add minimist to example testrun

--- example/testrun.js
+++ example/testrun.js
@@ -1,14 +1,20 @@
 // node -r esm testrun
-process.env.NODE_ENV = 'production';
 const { TMX2TODS } = require('./TMX2TODS');
+const minimist = require('minimist');
+process.env.NODE_ENV = 'production';
 
-const sourceDir = `./sourceFiles`;
-const targetDir = `./output`;
+const args = minimist(process.argv.slice(2), {
+  alias: { i: 'inDir', o: 'outDir', c: 'count', s: 'start' },
+  default: { inDir: './sourceFiles', outDir: './output' },
+});
+
+const sourceDir = args.sourceDir || `./sourceFiles`;
+const targetDir = args.targetDir || `./output`;
 
 TMX2TODS({
   disableProgress: true,
-  sourceDir,
-  targetDir,
-  // start: 441,
-  // count: 1,
+  targetDir: args.outDir,
+  sourceDir: args.inDir,
+  count: args.count,
+  start: args.start,
 });
